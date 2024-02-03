226b8e9ec2edf073464681d5e46f54e4af869f29
Abdul Latif Syafii <a.l.syafii@gmail.com>
2024-02-03 14:09:01 +0700

    User API

--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/bhill77/goshop/entity"
@@ -36,6 +37,57 @@ func (h UserHandler) Create(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, map[string]interface{}{"success": true, "data": user})
+}
+
+func (h UserHandler) Update(c echo.Context) error {
+	id := c.Param("id")
+
+	var user entity.User
+	err := h.db.Where("id", id).First(&user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return c.JSON(http.StatusNotFound, "Invalid id")
+	}
+
+	var payload entity.User
+	c.Bind(&payload)
+
+	user.Name = payload.Name
+	user.Address = payload.Address
+	user.PhoneNumber = payload.PhoneNumber
+
+	if err := h.db.Updates(&user).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, "gagal update")
+	}
+
+	return c.JSON(http.StatusOK, user)
+}
+
+func (h UserHandler) Delete(c echo.Context) error {
+	id := c.Param("id")
+
+	var user entity.User
+	err := h.db.Where("id", id).First(&user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return c.JSON(http.StatusNotFound, "Invalid id")
+	}
+
+	if err := h.db.Delete(&user).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, "gagal hapus")
+	}
+
+	return c.JSON(http.StatusOK, "user berhasil dihapus")
+}
+
+func (h UserHandler) Show(c echo.Context) error {
+	id := c.Param("id")
+
+	var user entity.User
+	err := h.db.Where("id", id).First(&user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return c.JSON(http.StatusNotFound, "Invalid id")
+	}
+
+	return c.JSON(http.StatusOK, user)
 }
 
 func HashPassword(password string) (string, error) {
